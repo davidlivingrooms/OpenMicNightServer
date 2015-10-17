94335b950a76c98a7fe471ef8b2d346e9edf081f
david <davidlivingrooms@gmail.com>
2015-10-17 17:51:13 -0600

    Added ability to create open mics.

--- conf/sql/create_tables.sql
+++ conf/sql/create_tables.sql
@@ -1,11 +1,18 @@
+
+CREATE TYPE weekday AS ENUM ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday');
+CREATE TYPE regularity AS ENUM ('weekly', 'bi-Weekly', 'monthly');
+
+
 CREATE TABLE openmic (
   id            SERIAL PRIMARY KEY,
-  openmic_name   varchar(120),
+  openmic_name      varchar(120),
+  openmic_weekday   weekday not null,
+  openmic_regularity   regularity not null,
   comedian      boolean not null,
   poet          boolean not null,
   musician      boolean not null,
   contact_email_address   varchar(120),
-  contact_phone_number   varchar(120),
+  contact_phone_number   varchar(20),
   venue_name   varchar(120),
   venue_address   varchar(120) not null,
   state   varchar(60) not null,
@@ -13,5 +20,6 @@ CREATE TABLE openmic (
   sign_up_time   varchar(10) not null,
   start_time   varchar(10) not null,
   is_free      boolean not null,
+  next_openmic_date date not null,
   notes   varchar(400)
 );
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,60 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+var moment = require('moment');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/openmicnight';
 
-router.get('/api/openmic/add', function(req, res, next) {
-  var params = req.query;
-  var data = {openmic_name: params.openMicName, comedian: params.comedian, poet: params.poet, musician: params.musician,
+router.post('/api/openmic/save', function(req, res, next) {
+  var params = req.body;
+  var data = {openmic_name: params.openMicName, openmic_weekday : params.openMicWeekDay,
+    openmic_regularity : params.openMicRegularity, comedian: params.comedians, poet: params.poets, musician: params.musicians,
     contact_email_address: params.contactEmailAddress, contact_phone_number: params.contactPhoneNumber,
     venue_name: params.venueName, venue_address: params.venueAddress, state: params.state, city: params.city,
-    sign_up_time: params.signUpTime, start_time: params.startTime, is_free: params.isFree, notes: params.notes};
+    sign_up_time: params.signUpTime, start_time: params.startTime, is_free: params.isOpenMicFree,
+    next_openmic_day: params.nextOpenMicDate, notes: params.notes};
 
-  var insertOpenMicStatement = 'INSERT INTO openmic(openmic_name, comedian, poet, musician, contact_email_address, '
-    + 'contact_phone_number, venue_name, venue_address, state, city, sign_up_time, start_time, is_free, notes) '
-    + 'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)';
+  var insertOpenMicStatement = 'INSERT INTO openmic(openmic_name, openmic_weekday, openmic_regularity, comedian, poet, ' +
+    'musician, contact_email_address, contact_phone_number, venue_name, venue_address, state, city, sign_up_time, ' +
+    'start_time, is_free, next_openmic_date, notes) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, ' +
+    '$14, $15, $16, $17)';
 
   pg.connect(connectionString, function(err, client, done) {
+    client.query(insertOpenMicStatement, [data.openmic_name, data.openmic_weekday, data.openmic_regularity,
+                                          data.comedian, data.poet, data.musician, data.contact_email_address,
+                                          data.contact_phone_number, data.venue_name, data.venue_address, data.state,
+                                          data.city, data.sign_up_time, data.start_time, data.is_free,
+                                          data.next_openmic_day, data.notes]);
+    if(err) {
+      console.log(err);
+    }
+  });
 
-    client.query(insertOpenMicStatement, [data.text, data.complete]);
+  res.json({ title: 'some open mic' });
+});
+
+//TODO they will pass in a single date with no time value. We should have a route that returns any open mics on that date. takes in a date and a city
+
+router.get('/api/openmic/list', function(req, res, next) {
+  var params = req.query;
+  var data = {city: params.city};
+  var selectOpenMicByCity = 'SELECT * FROM openmic WHERE city LIKE $1';
+  pg.connect(connectionString, function(err, client, done) {
+    var query = client.query(selectOpenMicByCity, ['%' + data.city + '%']);
+    var results = [];
+
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    query.on('end', function() {
+      client.end();
+      return res.json(results);
+    });
 
     if(err) {
       console.log(err);
     }
-
   });
-  //client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
-
-
-  res.json({ title: 'some open mic' });
 });
 
 module.exports = router;
